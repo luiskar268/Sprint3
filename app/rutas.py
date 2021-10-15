8102621761eb86b111f61569c9a8c1cede6a8d75
Your Name <you@example.com>
2021-10-14 23:58:00 -0500

    upload all

---
+++ app/rutas.py
@@ -0,0 +1,45 @@
+from main import app
+from flask import render_template, url_for
+
+
+
+@app.route('/')
+@app.route('/front_page')
+def front_page():
+    title= 'Social Melipona beecheii'
+    return render_template('front_page.html', title = title)
+
+@app.route('/dashboard')
+def dashboard():
+    title= 'Dashboard'
+    return render_template('dashboard.html', title = title)
+
+@app.route('/login')
+def login():
+    title= 'Login'
+    return render_template('login.html', title = title)
+
+@app.route('/locacion')
+def locacion():
+    title= 'locacion'
+    return render_template('locacion.html', title = title)
+
+@app.route('/comentario')
+def comentario():
+    title= 'Comentario'
+    return render_template('comentario.html', title = title)
+
+@app.route('/ayuda')
+def ayuda():
+    title= 'Ayuda'
+    return render_template('ayuda.html', title = title)
+
+@app.route('/profile')
+def profile():
+    title= 'Perfil de Usuario'
+    return render_template('profile.html', title = title)
+
+@app.route('/publicacion')
+def publicacion():
+    title= 'Publicación'
+    return render_template('publicacion.html', title = title)
